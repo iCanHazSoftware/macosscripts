15e887df16d0b2e9197116c267731231d6a227ee
Joe Rogers <131911173+iCanHazSoftware@users.noreply.github.com>
2024-02-12 22:23:33 -0500

    Create automaticupdates.sh
    Added Script

---
+++ AutomaticUpdates/automaticupdates.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist AutomaticCheckEnabled -bool TRUE
+
+defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist AutomaticDownload -bool TRUE
+
+defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist AutomaticallyInstallMacOSUpdates -bool TRUE
+
+defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist ConfigDataInstall -bool TRUE
+
+defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist CriticalUpdateInstall -bool TRUE
+
+#defaults write /Library/Preferences/com.apple.commerce.plist AutoUpdate -bool TRUE
+
+defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist ConfigDataInstall -bool TRUE
+
+defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist CriticalUpdateInstall -bool TRUE
